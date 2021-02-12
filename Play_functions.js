5643afde316de338a443d34a8365b010f353c273
CodinPetr <petrshalyapin@gmail.com>
2021-02-11 21:30:07 -0500

    added functions

--- Play_functions.js
+++ Play_functions.js
@@ -1,10 +1,47 @@
 var car = "Mustang";
 var speed = 55;
 
-function go(){
+function go(car){
     if (speed === 55) {
      console.log("Driving");
+    } else {
+        console.log("speeding")
     }
 }
+var speed = 65;
  return go();
 
+ class Car {
+    constructor(make, model, year){
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    }
+  }
+  
+  var car1 = new Car(
+    "Audi",
+    "Q5",
+    "2021"
+  );
+  
+  var speed = 55;
+  
+  function pickCar() {
+    console.log("I would like to pick a this car")
+    console.log(car1);
+  }
+  pickCar(Car);
+  
+  function go(){
+      if (speed === 55) {
+       console.log("Driving");
+      } else {
+          console.log("speeding");
+      }
+  }
+  
+  var speed = 65;
+   return go();
+
+   
